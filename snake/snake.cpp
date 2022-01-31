20cd8e4fa4d1be3cb5a4c4e426c8f6ff5dfb2a76
Hankerman <stalker-2204@mail.ru>
2022-01-31 20:45:42 +0300

    Добавьте файлы проекта.

---
+++ snake/snake.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+void fill(char arr[][100], int horizontale, int vertical) {
+
+    char wall = '#', empt = ' ';
+
+    for (int i = 0; i < vertical; i++) {
+        for (int j = 0; j < horizontale; j++) {
+            if (i == 0 || i == vertical - 1) {
+                arr[i][j] = wall;
+            }
+            else if (j == 0 || j == horizontale - 1) {
+                arr[i][j] = wall;
+            }
+            else {
+                arr[i][j] = empt;
+            }
+        }
+        
+    }
+}
+
+void print(char arr[][100], int horizontale, int vertical) {
+    for (int i = 0; i < vertical; i++) {
+        for (int j = 0; j < horizontale; j++) {
+            cout << arr[i][j];
+        }
+        cout << endl;
+    }
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    const int horizontal = 100, vertical = 25;
+    char pole[vertical][horizontal];
+    int n = 1;
+
+    fill(pole, horizontal, vertical);
+    //print(pole, horizontal, vertical);
+
+    while (n != 0) {
+        print(pole, horizontal, vertical);
+        Sleep(500);
+        system("cls");
+    }
+    
+    return 0;
+}
